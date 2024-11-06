bb777ba35fbd862b39ede98df3a704ddc139d733
Natalia Jabinschi <natalia.jabinschi@umontreal.ca>
2024-11-06 12:19:05 -0500

    Modifications du code pour ajuster le style de l'application et mieux intégrer la nouvelle identité visuelle de la marque.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('fr');
+    this.translate.use('fr'); // Vous pouvez changer la langue ici.
+    this.translate.get('PROJECT_TITLE').subscribe((title: string) => {
+      document.title = title;
+    });
+  }
 }
